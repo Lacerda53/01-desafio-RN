265a19a5e4d92e336468bee86cb223ab9d29cfdc
Lacerda53 <guilhermeandrade2013@gmail.com>
2022-02-01 00:57:42 -0300

    fixing create todo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,22 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+
+    const newValue: Task = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    }
+
+    setTasks(prevValue => [...prevValue, newValue])
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    setTasks(prevValue => prevValue.map(item => (item.id === id ? { ...item, done: !item.done } : { ...item })))
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(prevValue => prevValue.filter(item => item.id !== id))
   }
 
   return (
@@ -26,10 +33,10 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <TasksList 
-        tasks={tasks} 
+      <TasksList
+        tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
+        removeTask={handleRemoveTask}
       />
     </View>
   )
